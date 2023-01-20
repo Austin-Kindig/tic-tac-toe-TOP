6e10b2d89c2d869b2faa592a89772bc8c98c2f03
Austin Kindig <kindig.austin.ak@gmail.com>
2023-01-20 14:26:35 -0500

    add functionality to place appropriate marker on click event

--- script.js
+++ script.js
@@ -1,12 +1,31 @@
-const cell0 = document.getElementById('0')
+const cells = document.querySelectorAll('.game-cell')
+
 const gameBoard = (() => {
-    const cells = ['x','x','x','x','x','x','x','x','x']
+    const cells = []
     
     return {
         cells
     }
 })()
 
+document.addEventListener('click', (e) => {
+    const marker = xOrO()
+    gameBoard.cells[e.target.id] = marker
+    render()
+})
+
 function render() {
-    cell0.innerHTML = gameBoard.cells[0]
+    cells.forEach(cell => {
+        if (gameBoard.cells[cell.id] === undefined) {
+            cell.innerHTML = ''
+        } else {
+            cell.innerHTML = gameBoard.cells[cell.id]
+        }
+        
+    });
 }
+
+function xOrO() {
+    return (gameBoard.cells.filter(n=> n === 0 || n ).length % 2 === 0 ? 'x' : 'o') 
+}
+// 'x','x','x','x','x','x','x','x','x'
