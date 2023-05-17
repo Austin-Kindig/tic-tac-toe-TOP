cf3d99d428d53cb5883eb09693d68041461b7570
Austin-Kindig <kindig.austin.ak@gmail.com>
2023-05-17 06:38:26 -0900

    translate old functions into their respective modules

--- script.js
+++ script.js
@@ -1,20 +1,50 @@
 document.addEventListener('submit', (e) => {
     e.preventDefault()
+    Game.reset()
     let input = new FormData(document.querySelector('form'))
     input = Object.fromEntries(input)
     
     // create players
     let player1 = Player(input['player1-name'], input['player1-color'])
+    player1.marker = 'x'
     let player2 = Player(input['player2-name'], input['player2-color'])
+    player2.marker = 'o'
     document.querySelector('form').reset()
+    Game.displayPlayers()
     return {
         player1,
         player2
     }
 })
 
+const resetButton = document.querySelector('.reset-game')
+const clearButton = document.querySelector('.clear-game')
+resetButton.addEventListener('click', Game.reset())
+clearButton.addEventListener('click', GameBoard.clear())
+
+// need event listerner for clicking cells,
+
+
 // game control logic
 const Game = (() => {
+    let currentPlayer = 1
+    const playRound = (position) => {
+        if (currentPlayer === 1) {
+            GameBoard.placeMarker('x', position)
+            currentPlayer = 2
+            if (checkVictory === 'end') {
+                player1.score += 1
+            }
+        }
+        else {
+            GameBoard.placeMarker('x', position)
+            currentPlayer = 1
+            if (checkVictory === 'end') {
+                player2.score += 1
+            }
+        }
+        updateDisplay()
+    }
     // check to see if any row has 3 markers that are the same
     const horizontalCheck = () => {
         let board = gameBoard.getBoard()
@@ -58,215 +88,105 @@ const Game = (() => {
             return 'tie'
         }
     }
+    const displayPlayers = () => {
+        const player1Display = document.getElementById('player1')
+        const player1Score = document.getElementsByClassName('x-score')
+        const player2Display = document.getElementById('player2')
+        const player2Score = document.getElementsByClassName('o-score')
+
+        player1Display.innerText = player1.getName()
+        player1Display.style.color = player1.getColor()
+        player1Score.innerText = player1.getScore()
+        player2Display.innerText = player2.getName()
+        player2Display.style.color = player2.getColor()
+        player2Score.innerText = player2.getScore()
+    }
+    const reset = () => {
+        GameBoard.clear()
+        player1.resetScore()
+        player2.resetScore()
+        updateDisplay()
+    }
+    const updateDisplay = () => {
+        GameBoard.draw()
+        displayPlayers()
+    }
 
     return {
-        checkVictory
+        playRound,
+        checkVictory,
+        displayPlayers,
+        reset,
+        updateDisplay
     }
 })
 
 // game board module - holds the board array and
 // manages the markers on it.
 const GameBoard = (() => {
-    board: []
+    let board = []
 
     const placeMarker = (marker, position) => {
         board[position] = marker;
     }
-    const getBoard = () => {
-        return board;
+    const getBoard = () => board;
+    const draw = () => {
+        const cells = document.querySelectorAll('.game-cell')
+        cells.forEach(cell => {
+            if (gameBoard.cells[cell.id] === undefined) {
+                cell.innerHTML = ''
+            } else {
+                cell.innerHTML = board[cell.id]
+                if (cell.innerHTML === 'x') {
+                    cell.style.color = player1.color
+                } else {
+                    cell.style.color = player2.color
+                }
+            }
+        });
+    }
+    const clear = () => {
+        board = []
+        draw()
     }
 
     return {
         placeMarker,
-        getBoard
+        getBoard,
+        draw,
+        clear
     };
 })();
 
 // factory to create tic-tac-toe players
 const Player = (name, color) => {
-    name : name
-    color : color
-    score : 0
+    let score = 0
 
-    const getName = () => {
-        return name
-    }
-    const getColor = () => {
-        return color
-    }
-    const getScore = () => {
-        return score
-    }
-
+    const getName = () => name
+    const getColor = () => color
+    const getScore = () => score
     const incrementScore = () => {
         ++score
+    }
+    const resetScore = () => {
+        score = 0
     }
 
     return {
         getName,
         getColor,
         getScore,
-        incrementScore
+        incrementScore,
+        resetScore
     }
 }
-
-
-
-
-
-
 
 
 /*
-//const cells = document.querySelectorAll('.game-cell')
-const resetButton = document.querySelector('.reset-game')
-const clearButton = document.querySelector('.clear-game')
 //const board = document.querySelector('.game-board')
-const score1Display = document.querySelector('.x-score')
-const score2Display = document.querySelector('.o-score')
 const userMessage = document.querySelector('.user-message')
-
-
-function populateData(input) {
-    const player1 = document.getElementById('player1')
-    const player2 = document.getElementById('player2')
-
-    player1.innerText = input['player1-name']
-    player1.style.color = input['player1-color']
-    player2.innerText = input['player2-name']
-    player2.style.color = input['player2-color']
-}
-
-resetButton.addEventListener('click', resetGame)
-clearButton.addEventListener('click', clearBoard)
-
-function resetGame() {
-    clearBoard()
-    resetScores()
-}
-function resetScores() {
-    gameBoard.player1Score = 0
-    gameBoard.player2Score = 0
-}
-
-function clearBoard() {
-    gameBoard.cells = []
-    render()
-}
-
-const gameBoard = (() => {
-    const cells = []
-    const player1Score = 0
-    const player2Score = 0
-    
-    return {
-        cells,
-        player1Score,
-        player2Score
-    }
-})()
-
-board.addEventListener('click', (e) => {
-    if (gameBoard.cells[e.target.id] === undefined) {
-        const marker = xOrO()
-        gameBoard.cells[e.target.id] = marker
-        render()
-        if(victoryCheck() === 'x') {
-            displayMessage(`${temp['player1-name']} wins`)
-            incrementScore('x')
-            return
-        }
-        if(victoryCheck() === 'o') {
-            displayMessage(`${temp['player2-name']} wins`)
-            incrementScore('o')
-            return
-        }
-        if(scratchCheck() === true) {
-            displayMessage('Scratch!')
-        }
-    }
-    
-})
-
-function incrementScore(winner) {
-    if (winner === 'x') {
-        gameBoard.player1Score += 1
-        score1Display.innerHTML = gameBoard.player1Score
-
-    } else {
-        gameBoard.player2Score += 1
-        score2Display.innerHTML = gameBoard.player2Score
-    }
-    clearBoard()
-
-}
-
-function render(input) {
-    cells.forEach(cell => {
-        if (gameBoard.cells[cell.id] === undefined) {
-            cell.innerHTML = ''
-        } else {
-            cell.innerHTML = gameBoard.cells[cell.id]
-            if (cell.innerHTML === 'x') {
-                cell.style.color = temp['player1-color']
-            } else {
-                cell.style.color = temp['player2-color']
-            }
-        }
-        
-    });
-}
-
 
 function xOrO() {
     return (gameBoard.cells.filter(n=> n === 0 || n ).length % 2 === 0 ? 'x' : 'o') 
 }
 
-function generateCheckLine(start, add) {
-    const line = []
-    line.push(gameBoard.cells[start])
-    line.push(gameBoard.cells[start + add])
-    line.push(gameBoard.cells[start + add + add])
-    return line
-}
-
-function generateLinesArray() {
-    const array = [
-        generateCheckLine(0,1),
-        generateCheckLine(3,1),
-        generateCheckLine(6,1),
-        generateCheckLine(0,3),
-        generateCheckLine(1,3),
-        generateCheckLine(2,3),
-        generateCheckLine(0,4),
-        generateCheckLine(2,2)
-    ]
-    return array
-}
-
-function victoryCheck() {
-    const array = generateLinesArray()
-    const xWin = 'xxx'
-    const oWin = 'ooo'
-    let victory = ''
-    
-    array.forEach(element => {
-        if (element.join('') === xWin) {
-            victory = 'x'
-        }
-        if (element.join('') === oWin) {
-            victory = 'o'
-        }
-    })
-    return victory
-}
-
-function scratchCheck() {
-    if(gameBoard.cells.filter(n=> n === 0 || n ).length === 9) {
-        return true
-    }
-}
-
-function displayMessage(message) {
-    userMessage.innerText = message
-}*/
